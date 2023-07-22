9e1ddd4896142d1a030984c0062b24b65c579b2c
Brian Sunter <2816289+briansunter@users.noreply.github.com>
2023-07-21 15:55:15 -1000

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
+import JSZip from 'jszip';
+import { saveAs } from 'file-saver';
+
+const ffmpeg = createFFmpeg({ log: true });
 
 function App() {
+  const [ready, setReady] = useState(false);
+
+  const load = async () => {
+    await ffmpeg.load();
+    setReady(true);
+  };
+
+  useEffect(() => {
+    load();
+  }, []);
+
+  const convertAndSplit = async (file:File) => {
+    // Read the file
+    ffmpeg.FS('writeFile', 'test.m4b', await fetchFile(file));
+
+    // Convert to MP3
+    await ffmpeg.run('-i', 'test.m4b', '-codec:a', 'libmp3lame', '-qscale:a', '2', 'output.mp3');
+
+    // Split the MP3
+    await ffmpeg.run('-i', 'output.mp3', '-f', 'segment', '-segment_time', '600', '-c', 'copy', 'out%03d.mp3');
+
+    // Create a new JSZip instance
+    const zip = new JSZip();
+
+    // Iterate over the chunks
+    const chunkFiles = ffmpeg.FS('readdir', '/').filter(file => file.startsWith('out'));
+    for (let file of chunkFiles) {
+      // Read the chunk
+      const data = ffmpeg.FS('readFile', file);
+
+      // Add the chunk to the ZIP
+      zip.file(file, data);
+    }
+
+    // Generate the ZIP file
+    const zipFile = await zip.generateAsync({ type: 'blob' });
+
+    // Download the ZIP file
+    saveAs(zipFile, 'chunks.zip');
+  };
+
+  if (!ready) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="file" onChange={(e) => {
+        if (!e.target.files) return;
+        convertAndSplit(e.target.files[0])}} />
     </div>
   );
 }
